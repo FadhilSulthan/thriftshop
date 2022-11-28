4b16cf2ff8159bce8cb00dc1943b7281753df748
FadhilSulthan <fadhilsulthan1212@gmail.com>
2022-11-29 00:09:31 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/home";
 import ProductDetail from "./pages/detail";
 import About from "./pages/about";
 import Apparel from "./pages/apparel";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
   return (
     <div className="header">
-      <div className="containernav">
+      <div className="container-header">
         <img className="logo" src="logo.png" alt="logo" />
-        {/* navlink untuk berpindah route/halaman */}
-        <NavLink to="/home" className="navbar">
-          Home
-        </NavLink>
-        <NavLink to="/explore" className="navbar">
-          Explore
-        </NavLink>
-        <NavLink to="/apparel" className="navbar">
-          Apparel
-        </NavLink>
-        <NavLink to="/about" className="navbar">
-          About
-        </NavLink>
+        <h2 className="title">Thrift Shop</h2>
       </div>
     </div>
   );
